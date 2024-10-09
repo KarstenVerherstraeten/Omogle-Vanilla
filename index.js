825664881f61f0b5fb9c91cd8d020e15c551e3b4
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-10-09 14:23:12 +0200

    changed display message

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ wss.on("connection", function connection(ws) {
   ws.on("message", function message(data) {
     console.log("received: %s", data);
   });
-  ws.send("something");
+  ws.send("Welcome to the WebSocket server!");
 });
