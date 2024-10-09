cc30674974200f58fc083420a782322b2dd95a8a
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-10-09 14:41:50 +0200

    Refactor WebSocket server to handle multiple clients and broadcast messages

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+const clients = new Map();
 
-wss.on("connection", function connection(ws) {
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
-  ws.send("Welcome to the WebSocket server!");
+wss.on('connection', function connection(ws) {
+    const id = Date.now();
+    clients.set(id, ws);
+
+    ws.on("message", function message(data) {
+        console.log("received: %s", data);
+
+
+        let message = data.toString();
+        // Broadcast the message to all other clients
+        clients.forEach((client, clientId) => {
+            if (clientId !== id) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        clients.delete(id);
+    });
+
+    ws.send("Welcome to the WebSocket server!");
 });
